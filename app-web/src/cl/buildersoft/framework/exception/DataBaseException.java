219a00ee2aef3aec075ea646010e4390f9422d66
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2012-03-22 03:03:40 +0000

    Se agrega variable serialVersionUID

--- app-web/src/cl/buildersoft/framework/exception/DataBaseException.java
+++ app-web/src/cl/buildersoft/framework/exception/DataBaseException.java
@@ -4,6 +4,7 @@ import cl.buildersoft.framework.util.Messages;
 
 
 public class DataBaseException extends RuntimeException {
+	private static final long serialVersionUID = -614107503606909020L;
 
 	public DataBaseException(Throwable t) {
 		super(Messages.getString("ErrorHandler.BaseDatosError"));
