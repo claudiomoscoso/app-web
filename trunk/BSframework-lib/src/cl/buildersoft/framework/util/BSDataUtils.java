396d66cd76515ac6d61102e7abff8976a6317abf
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2012-03-22 04:18:29 +0000

    Se cambian las excepciones por RuntimeException

--- trunk/BSframework-lib/src/cl/buildersoft/framework/util/BSDataUtils.java
+++ trunk/BSframework-lib/src/cl/buildersoft/framework/util/BSDataUtils.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,19 +13,29 @@ import javax.servlet.ServletContext;
 public class BSDataUtils extends BSUtils {
 	PreparedStatement preparedStatement = null;
 
-	protected void closeSQL(ResultSet rs) throws SQLException {
+	protected void closeSQL(ResultSet rs) throws RuntimeException {
 		if (rs != null) {
-			rs.close();
+			try {
+				rs.close();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+
 		}
 		closeSQL();
 	}
 
-	protected void closeSQL() throws SQLException {
-		this.preparedStatement.close();
+	protected void closeSQL() throws RuntimeException {
+		try {
+			this.preparedStatement.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 
 	protected Integer update(Connection conn, String sql, Object parameter)
-			throws SQLException {
+			throws RuntimeException {
 		List<Object> prms = new ArrayList<Object>();
 		prms.add(parameter);
 
@@ -34,36 +43,43 @@ public class BSDataUtils extends BSUtils {
 	}
 
 	protected Integer update(Connection conn, String sql, List<Object> parameter)
-			throws SQLException {
+			throws RuntimeException {
 		int rowsAffected = 0;
-
-		preparedStatement = conn.prepareStatement(sql);
-		parametersToStatement(parameter, preparedStatement);
-		rowsAffected = preparedStatement.executeUpdate();
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			parametersToStatement(parameter, preparedStatement);
+			rowsAffected = preparedStatement.executeUpdate();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 
 		return rowsAffected;
 	}
 
 	protected Long insert(Connection conn, String sql, List<Object> parameter)
-			throws SQLException {
+			throws RuntimeException {
 		Long newKey = 0L;
 
-		preparedStatement = conn.prepareStatement(sql,
-				Statement.RETURN_GENERATED_KEYS);
-		parametersToStatement(parameter, preparedStatement);
-		newKey = (long) preparedStatement.executeUpdate();
+		try {
+			preparedStatement = conn.prepareStatement(sql,
+					Statement.RETURN_GENERATED_KEYS);
+			parametersToStatement(parameter, preparedStatement);
+			newKey = (long) preparedStatement.executeUpdate();
 
-		ResultSet rs = preparedStatement.getGeneratedKeys();
-		if (rs.next()) {
-			newKey = rs.getLong(1);
+			ResultSet rs = preparedStatement.getGeneratedKeys();
+			if (rs.next()) {
+				newKey = rs.getLong(1);
+			}
+			rs.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-		rs.close();
 
 		return newKey;
 	}
 
 	protected String queryField(Connection conn, String sql, Object parameter)
-			throws SQLException {
+			throws RuntimeException {
 		List<Object> prms = new ArrayList<Object>();
 		prms.add(parameter);
 
@@ -71,20 +87,22 @@ public class BSDataUtils extends BSUtils {
 	}
 
 	protected String queryField(Connection conn, String sql,
-			List<Object> parameter) throws SQLException {
+			List<Object> parameter) throws RuntimeException {
 		String out = null;
 		ResultSet rs = queryResultSet(conn, sql, parameter);
-
-		if (rs.next()) {
-			out = rs.getString(1);
+		try {
+			if (rs.next()) {
+				out = rs.getString(1);
+			}
+			rs.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-		rs.close();
-
 		return out;
 	}
 
 	protected ResultSet queryResultSet(Connection conn, String sql,
-			Object parameter) throws SQLException {
+			Object parameter) throws RuntimeException {
 		List<Object> prms = new ArrayList<Object>();
 		prms.add(parameter);
 
@@ -92,72 +110,79 @@ public class BSDataUtils extends BSUtils {
 	}
 
 	protected ResultSet queryResultSet(Connection conn, String sql,
-			List<Object> parameters) throws SQLException {
+			List<Object> parameters) throws RuntimeException {
 		ResultSet out = null;
 
-		preparedStatement = conn.prepareStatement(sql);
-		parametersToStatement(parameters, preparedStatement);
-
-		out = preparedStatement.executeQuery();
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			parametersToStatement(parameters, preparedStatement);
+			out = preparedStatement.executeQuery();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 
 		return out;
 	}
 
 	private void parametersToStatement(List<Object> parameters,
-			PreparedStatement preparedStatement) throws SQLException {
+			PreparedStatement preparedStatement) throws RuntimeException {
 		// this.callableStatement = conn.prepareCall(sqlStatement);
-		if (parameters != null) {
-			// int len = parameters.size();
-			int i = 0;
-			// Object param = null;
-			// for (int i = 0; i < len; i++) {
-			// param = parameters[i];
-			for (Object param : parameters) {
+		try {
+			if (parameters != null) {
+				// int len = parameters.size();
+				int i = 0;
+				// Object param = null;
+				// for (int i = 0; i < len; i++) {
+				// param = parameters[i];
+				for (Object param : parameters) {
 
-				if (param instanceof String) {
-					preparedStatement.setString(i + 1, (String) param);
-				} else if (param instanceof Integer) {
-					preparedStatement.setInt(i + 1,
-							((Integer) param).intValue());
-				} else if (param instanceof Double) {
-					preparedStatement.setDouble(i + 1,
-							((Double) param).doubleValue());
-				} else if (param instanceof Long) {
-					preparedStatement
-							.setLong(i + 1, ((Long) param).longValue());
-				} else if (param instanceof Boolean) {
-					preparedStatement.setBoolean(i + 1,
-							((Boolean) param).booleanValue());
-				} else if (param instanceof java.util.Calendar
-						|| param instanceof java.util.GregorianCalendar) {
-					java.sql.Timestamp time = new java.sql.Timestamp(
-							((java.util.Calendar) param).getTimeInMillis());
-					preparedStatement.setTimestamp(i + 1, time);
-				} else if (param instanceof java.util.Date) {
-					java.sql.Timestamp time = new java.sql.Timestamp(
-							((java.util.Date) param).getTime());
-					preparedStatement.setTimestamp(i + 1, time);
-				} else if (param == null) {
-					preparedStatement.setNull(i + 1, java.sql.Types.NULL);
-				} else {
-					String message = "Object type not cataloged, please insert code in \"AbstractProcess\" for class \""
-							+ param.getClass().getName() + "\"";
+					if (param instanceof String) {
+						preparedStatement.setString(i + 1, (String) param);
+					} else if (param instanceof Integer) {
+						preparedStatement.setInt(i + 1,
+								((Integer) param).intValue());
+					} else if (param instanceof Double) {
+						preparedStatement.setDouble(i + 1,
+								((Double) param).doubleValue());
+					} else if (param instanceof Long) {
+						preparedStatement.setLong(i + 1,
+								((Long) param).longValue());
+					} else if (param instanceof Boolean) {
+						preparedStatement.setBoolean(i + 1,
+								((Boolean) param).booleanValue());
+					} else if (param instanceof java.util.Calendar
+							|| param instanceof java.util.GregorianCalendar) {
+						java.sql.Timestamp time = new java.sql.Timestamp(
+								((java.util.Calendar) param).getTimeInMillis());
+						preparedStatement.setTimestamp(i + 1, time);
+					} else if (param instanceof java.util.Date) {
+						java.sql.Timestamp time = new java.sql.Timestamp(
+								((java.util.Date) param).getTime());
+						preparedStatement.setTimestamp(i + 1, time);
+					} else if (param == null) {
+						preparedStatement.setNull(i + 1, java.sql.Types.NULL);
+					} else {
+						String message = "Object type not cataloged, please insert code in \"AbstractProcess\" for class \""
+								+ param.getClass().getName() + "\"";
 
-					throw new RuntimeException(message);
+						throw new RuntimeException(message);
+					}
+
+					i++;
 				}
-
-				i++;
 			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
 	}
 
 	public Connection getConnection(ServletContext context)
-			throws ClassNotFoundException, SQLException {
+			throws RuntimeException {
 		return getConnection(context, "cosoav");
 	}
 
 	public Connection getConnection(ServletContext context, String prefix)
-			throws ClassNotFoundException, SQLException {
+			throws RuntimeException {
 
 		String driverName = context.getInitParameter(prefix
 				+ ".database.driver");
@@ -176,13 +201,18 @@ public class BSDataUtils extends BSUtils {
 
 	public Connection getConnection(String driverName, String serverName,
 			String database, String password, String username)
-			throws ClassNotFoundException, SQLException {
+			throws RuntimeException {
+
 		Connection connection = null;
-		Class.forName(driverName);
+		try {
+			Class.forName(driverName);
+			String url = "jdbc:mysql://" + serverName + "/" + database;
 
-		String url = "jdbc:mysql://" + serverName + "/" + database;
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 
-		connection = DriverManager.getConnection(url, username, password);
 		return connection;
 	}
 }
