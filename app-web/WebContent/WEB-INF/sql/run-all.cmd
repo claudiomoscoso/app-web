a8de3e4ab168879f8b7b3aebba629f73b5799015
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2012-04-05 02:53:54 +0000

    Cambio en la secuencia de ejecución.

--- app-web/WebContent/WEB-INF/sql/run-all.cmd
+++ app-web/WebContent/WEB-INF/sql/run-all.cmd
@@ -1,4 +1,5 @@
 mysql -u root -p12870668 < create-bsframework.sql.txt
-mysql -u root -p12870668 < create-bscommon.sql.txt
 
 for %%i in (create-sys*.sql.txt) do mysql -u root -p12870668 < %%i
+
+mysql -u root -p12870668 < create-bscommon.sql.txt
