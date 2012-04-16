2fd57796b6a6e88b6a50dd8c220ad57974bd2384
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2012-04-16 18:52:30 +0000

    Se cambia la clave de la DB

--- trunk/app-web/WebContent/WEB-INF/sql/run-all.cmd
+++ trunk/app-web/WebContent/WEB-INF/sql/run-all.cmd
@@ -9,14 +9,14 @@ rem cargar datos para prueba
 @echo on
 
 cls
-mysql -u root -p12870668 < delete-tables.sql.txt
-mysql -u root -p12870668 < create-bsframework.sql.txt
-mysql -u root -p12870668 < create-bscommon.sql.txt
+mysql -u root -padmin < delete-tables.sql.txt
+mysql -u root -padmin < create-bsframework.sql.txt
+mysql -u root -padmin < create-bscommon.sql.txt
 
-for %%i in (create-db*.sql.txt) do mysql -u root -p12870668 < %%i
-for %%i in (create-rules*.sql.txt) do mysql -u root -p12870668 < %%i
-for %%i in (create-sp*.sql.txt) do mysql -u root -p12870668 < %%i
-for %%i in (create-data*.sql.txt) do mysql -u root -p12870668 < %%i
+for %%i in (create-db*.sql.txt) do mysql -u root -padmin < %%i
+for %%i in (create-rules*.sql.txt) do mysql -u root -padmin < %%i
+for %%i in (create-sp*.sql.txt) do mysql -u root -padmin < %%i
+for %%i in (create-data*.sql.txt) do mysql -u root -padmin < %%i
 
-mysql -u root -p12870668 < create-menu.sql.txt
+mysql -u root -padmin < create-menu.sql.txt
 
