43e4d3fb7ebd7bc710a54b48a11e2183f4543a62
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2012-07-16 02:53:29 +0000

    Se agrega el constructor de BSException que recibe Exception

--- BSframework-lib/src/cl/buildersoft/framework/exception/BSConfigurationException.java
+++ BSframework-lib/src/cl/buildersoft/framework/exception/BSConfigurationException.java
@@ -6,6 +6,9 @@ package cl.buildersoft.framework.exception;
 public class BSConfigurationException extends BSException {
 
 	private static final long serialVersionUID = -6054613850007173438L;
+	public BSConfigurationException(Exception e) {
+		super(e);
+	}
 
 	public BSConfigurationException(String code) {
 		super(code);
