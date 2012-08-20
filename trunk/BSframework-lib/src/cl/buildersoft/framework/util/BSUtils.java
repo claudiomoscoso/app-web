4632295a825096d4a4b78ee2f14eb61a1e295c85
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2012-08-20 17:58:41 +0000

    Se sacan los métodos de la clase BSUtils referentes a fechas.

--- trunk/BSframework-lib/src/cl/buildersoft/framework/util/BSUtils.java
+++ trunk/BSframework-lib/src/cl/buildersoft/framework/util/BSUtils.java
@@ -32,51 +32,6 @@ public class BSUtils {
 		return out;
 	}
 
-	public static String calendar2String(Calendar calendar) {
-		String out = calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH) + 1) + "-"
-				+ calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.HOUR_OF_DAY) + ":"
-				+ calendar.get(Calendar.MINUTE) + " - " + (new Date(calendar.getTimeInMillis()).toString());
-		return out;
-	}
+	
 
-	public static Calendar date2Calendar(java.util.Date date) {
-		Calendar out = Calendar.getInstance();
-		out.setTimeInMillis(date.getTime());
-		return out;
-	}
-
-	public static Calendar string2Calendar(String dateString, String format) {
-		DateFormat formatter = new SimpleDateFormat(format);
-		Calendar out = null;
-		try {
-			java.util.Date date = (java.util.Date) formatter.parse(dateString);
-			out = date2Calendar(date);
-			date = null;
-		} catch (ParseException e) {
-			throw new BSProgrammerException(e);
-		}
-		// System.out.println(calendar2String(out));
-
-		return out;
-	}
-
-	public static Boolean isValidDate(String date, String format) {
-		Boolean out = Boolean.TRUE;
-		SimpleDateFormat sdf = new SimpleDateFormat(format);
-
-		Date testDate = null;
-
-		try {
-			testDate = sdf.parse(date);
-		} catch (ParseException e) {
-			out = Boolean.FALSE;
-		}
-
-		if (!sdf.format(testDate).equals(date)) {
-			out = Boolean.FALSE;
-		}
-
-		return out;
-
-	}
 }
