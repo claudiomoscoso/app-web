eca32adb4e80f0aaea028c69d319989eb887c24a
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2013-12-12 16:29:12 +0000

    Se corrigen script de base de datos para dejarlos genéricos.

--- trunk/app-web/WebContent/WEB-INF/sql/M1/run-all.sh
+++ trunk/app-web/WebContent/WEB-INF/sql/M1/run-all.sh
@@ -7,24 +7,28 @@ echo 'Borrando tablas desestimadas... (erase-deprecated-object.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < erase-deprecated-object.sql.txt
 echo 'Creando tablas de plataforma... (create-bsframework.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < create-bsframework.sql.txt
-echo 'Creando tablas de sistema... (create-timectrl.sql.txt)'
-	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < create-timectrl.sql.txt
+#echo 'Creando tablas de plataforma... (create-bsframework.sql.sh)'	
+#	./create-bsframework.sql.sh $1
+	
+echo 'Creando tablas de sistema... (create-bsee.sql.txt)'
+	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < create-bsee.sql.txt
 	
 echo 'Creando reglas de plataforma...(rules-bsframework.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < rules-bsframework.sql.txt
-echo 'Creando reglas de sistema...(rules-timectrl.sql.txt)'
-	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < rules-timectrl.sql.txt
+echo 'Creando reglas de sistema...(rules-bsee.sql.txt)'
+	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < rules-bsee.sql.txt
 	
 echo 'Creando funciones de sistema...'
 	for i in $(ls fn*.sql.txt); do echo "> Running $i..."; mysql -D$1 -u root -padmin < $i; done
 echo 'Creando procedimientos de sistema...'
 	for i in $(ls sp*.sql.txt); do echo "> Running $i..."; mysql -D$1 -u root -padmin < $i; done
 	
-echo 'Creando datos básicos de plataforma...(data-bsframework.sql.txt)'
-	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-bsframework.sql.txt
+echo 'Creando datos básicos de plataforma...(data-bsframework.sql.sh)'
+	#mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-bsframework.sql.txt
+	./data-bsframework.sql.sh $1
 	
-echo 'Creando datos básicos del sistema...(data-timectrl.sql.txt)'
-	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-timectrl.sql.txt
+echo 'Creando datos básicos del sistema...(data-bsee.sql.txt)'
+	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-bsee.sql.txt
 	
 echo 'Creando opciones de menú...(data-menu.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-menu.sql.txt
