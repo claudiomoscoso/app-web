371aca34241b0ec2df036e77655d0bf353eba392
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2014-04-21 03:13:58 +0000

    Primera coneccion de workflow a la base de datos usando un data source.

--- trunk/app-web/src/cl/buildersoft/business/workflow/beans/Instance.java
+++ trunk/app-web/src/cl/buildersoft/business/workflow/beans/Instance.java
@@ -1,20 +1,33 @@
 package cl.buildersoft.business.workflow.beans;
 
-import java.util.List;
-
 import cl.buildersoft.framework.beans.BSBean;
 
 public class Instance extends BSBean {
 	private static final long serialVersionUID = 5970049877943399494L;
-	private Long idFlow = null;
+	private String TABLE = "tInstance";
+	private Long id = null;
+	private Long flow = null;
+	private Long creator = null;
 	private Long assigned = null;
-	private List<FieldFlow> fields = null;
+	private Long step = null;
 	
-	public Long getIdFlow() {
-		return idFlow;
+	public Long getId() {
+		return id;
 	}
-	public void setIdFlow(Long idFlow) {
-		this.idFlow = idFlow;
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getFlow() {
+		return flow;
+	}
+	public void setFlow(Long flow) {
+		this.flow = flow;
+	}
+	public Long getCreator() {
+		return creator;
+	}
+	public void setCreator(Long creator) {
+		this.creator = creator;
 	}
 	public Long getAssigned() {
 		return assigned;
@@ -22,10 +35,11 @@ public class Instance extends BSBean {
 	public void setAssigned(Long assigned) {
 		this.assigned = assigned;
 	}
-	public List<FieldFlow> getFields() {
-		return fields;
+	public Long getStep() {
+		return step;
 	}
-	public void setFields(List<FieldFlow> fields) {
-		this.fields = fields;
+	public void setStep(Long step) {
+		this.step = step;
 	}
+
 }
