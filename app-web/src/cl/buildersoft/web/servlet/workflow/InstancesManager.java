1d084ede6f6f422fc01262fcf2a20e569278f557
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2014-04-22 03:00:08 +0000

    Primeros Mantenedores

---
+++ app-web/src/cl/buildersoft/web/servlet/workflow/InstancesManager.java
@@ -0,0 +1,25 @@
+package cl.buildersoft.web.servlet.workflow;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import cl.buildersoft.framework.beans.BSTableConfig;
+import cl.buildersoft.web.servlet.common.BSHttpServlet;
+
+@WebServlet("/servlet/workflow/InstancesManager")
+             
+public class InstancesManager extends BSHttpServlet {
+	private static final long serialVersionUID = 583201704776707035L;
+
+	@Override
+	protected BSTableConfig getBSTableConfig(HttpServletRequest request) {
+		BSTableConfig table = initTable(request, "tInstance");
+
+		table.removeAction("DELETE");
+		table.removeAction("INSERT");
+				table.removeAction("EDIT");
+		
+		return table;
+	}
+
+}
