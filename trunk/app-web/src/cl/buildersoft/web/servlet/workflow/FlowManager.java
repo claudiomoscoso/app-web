d0dd4589d40866d70a3a2946e62b383b8ed027fc
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2014-04-22 03:00:08 +0000

    Primeros Mantenedores

--- trunk/app-web/src/cl/buildersoft/web/servlet/workflow/FlowManager.java
+++ trunk/app-web/src/cl/buildersoft/web/servlet/workflow/FlowManager.java
@@ -1,26 +1,17 @@
 package cl.buildersoft.web.servlet.workflow;
 
-import java.util.List;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 
-import cl.buildersoft.business.workflow.beans.Flow;
-import cl.buildersoft.business.workflow.service.Workflow;
-import cl.buildersoft.business.workflow.service.impl.WorkflowImpl;
 import cl.buildersoft.framework.beans.BSTableConfig;
 import cl.buildersoft.web.servlet.common.BSHttpServlet;
 
-@WebServlet("/servlet/workflow/FlowDefManager")
-public class FlowDefManager extends BSHttpServlet {
+@WebServlet("/servlet/workflow/FlowManager")
+public class FlowManager extends BSHttpServlet {
 	private static final long serialVersionUID = 5031905095957507242L;
 
 	@Override
 	protected BSTableConfig getBSTableConfig(HttpServletRequest request) {
-
-		Workflow wf = new WorkflowImpl();
-
-		List<Flow> list = wf.getFlows();
 
 		BSTableConfig table = initTable(request, "tFlow");
 		table.setTitle("Definición de Flujos");
