fdf80492f6bbbebd8dcd5474ca1ebb62e5ab0a1f
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2014-04-29 03:11:29 +0000

    Creacion de instancia

--- trunk/app-web/src/cl/buildersoft/web/servlet/workflow/InstancesManager.java
+++ trunk/app-web/src/cl/buildersoft/web/servlet/workflow/InstancesManager.java
@@ -3,7 +3,9 @@ package cl.buildersoft.web.servlet.workflow;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 
+import cl.buildersoft.framework.beans.BSAction;
 import cl.buildersoft.framework.beans.BSTableConfig;
+import cl.buildersoft.framework.type.BSActionType;
 import cl.buildersoft.web.servlet.common.BSHttpServlet;
 
 @WebServlet("/servlet/workflow/InstancesManager")
@@ -17,6 +19,18 @@ public class InstancesManager extends BSHttpServlet {
 		table.removeAction("DELETE");
 		table.removeAction("INSERT");
 		table.removeAction("EDIT");
+
+		table.setTitle("Flujos en ejecución");
+		table.getField("cFlow").setLabel("Flujo");
+		table.getField("cCreator").setLabel("Creador");
+		table.getField("cAssigned").setLabel("Asignado");
+		table.getField("cStep").setLabel("Etapa");
+		
+		
+		BSAction action = new BSAction("START", BSActionType.Table);
+		action.setLabel("Nuevo");
+		action.setUrl("/servlet/workflow/Start");
+		table.addAction(action);
 
 		return table;
 	}
