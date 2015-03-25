343cdb564fd91d55096f93cc63ac0349f58e867c
claudio.moscoso <claudio.moscoso@51589d18-f4d8-1b4d-95e1-59229dae5c54>
2015-03-25 03:51:06 +0000

    Se permite modificar datos de la configuracion

--- trunk/BSframework-lib/src/cl/buildersoft/framework/util/BSConfig.java
+++ trunk/BSframework-lib/src/cl/buildersoft/framework/util/BSConfig.java
@@ -1,27 +1,53 @@
 package cl.buildersoft.framework.util;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BSConfig extends BSDataUtils {
 	public String getString(Connection conn, String key) {
 		return getValue(conn, key);
 	}
 
+	public void setString(Connection conn, String key, String value) {
+		setValue(conn, key, value);
+	}
+
 	public Integer getInteger(Connection conn, String key) {
 		return Integer.parseInt(getValue(conn, key));
+	}
+
+	public void setInteger(Connection conn, String key, Integer value) {
+		setValue(conn, key, value.toString());
 	}
 
 	public Double getDouble(Connection conn, String key) {
 		return Double.parseDouble(getValue(conn, key));
 	}
 
+	public void setDouble(Connection conn, String key, Double value) {
+		setValue(conn, key, value.toString());
+	}
+
 	public Boolean getBoolean(Connection conn, String key) {
 		return Boolean.parseBoolean(getValue(conn, key));
+	}
+
+	public void setBoolean(Connection conn, String key, Boolean value) {
+		setValue(conn, key, value.toString());
 	}
 
 	private String getValue(Connection conn, String key) {
 		String sql = "SELECT cValue FROM tParameter WHERE cKey=?";
 		return super.queryField(conn, sql, key);
+	}
+
+	private void setValue(Connection conn, String key, String value) {
+		String sql = "UPDATE tParameter SET cValue=? WHERE cKey=?";
+		List<Object> parameter = new ArrayList<Object>();
+		parameter.add(value);
+		parameter.add(key);
+		super.update(conn, sql, parameter);
 	}
 
 	public char getCSVSeparator(Connection conn) {
